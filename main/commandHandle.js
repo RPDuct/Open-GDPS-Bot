c665bb248b181bd2c642aa2d4cb38fc6cd24e11d
SMJSGaming <38814077+SMJSGaming@users.noreply.github.com>
2019-11-29 09:48:21 +0100

    Cleanup

--- main/commandHandle.js
+++ main/commandHandle.js
@@ -1,47 +1,47 @@
-const process = require("process");
-const help = require('./commands/help.js');
-const leaderboard = require('./commands/leaderboard.js');
-const license = require('./commands/license.js');
-const userfetch = require('./commands/userfetch.js');
-const comments = require('./commands/comments.js');
-const level = require('./commands/seelevel.js');
-const kill = require('./commands/owner/kill.js');
-const reboot = require('./commands/owner/reboot.js');
-
 module.exports = {
-    checkcommand: function(message, client, config) {
-        switch (message.content.split(config.prefix)[1]) {
-            case "test":
-                if (message.author.id == config.ownerid) {
-                    message.channel.send("I work!");
-                } else {
-                    message.channel.send(config.error.owner + "<@" + message.author.id + ">");
+    handle: function(message) {
+        require('./../bg/construct-config.js').build.then(function(config) {
+            if (typeof config !== 'undefined') {
+                if (message.author.bot) return;
+                if (message.content.startsWith(config.prefix)) {
+                    switch (message.content.split(config.prefix)[1]) {
+                        case "test":
+                            if (message.author.id == config.ownerid) {
+                                message.channel.send("I work!");
+                            } else {
+                                message.channel.send(config.error.owner + "<@" + message.author.id + ">");
+                            }
+                            break;
+                        case "license":
+                            require('./commands/license.js').start(message, config);
+                            break;
+                        case "kill":
+                            require('./commands/owner/kill.js').start(message, process, config);
+                            break;
+                        case "reboot":
+                            require('./commands/owner/reboot.js').start(message, client, config);
+                            break;
+                        case "version":
+                            require('./commands/owner/versioncheck.js').start(message, config);
+                            break;
+                    }
+                    if (message.content.startsWith(config.prefix + "leaderboard")) {
+                        require('./commands/leaderboard.js').start(message, config);
+                    }
+                    if (message.content.startsWith(config.prefix + "help")) {
+                        require('./commands/help.js').start(message, config);
+                    }
+                    if (message.content.startsWith(config.prefix + "user")) {
+                        require('./commands/userfetch.js').start(message, config);
+                    }
+                    if (message.content.startsWith(config.prefix + "comments")) {
+                        require('./commands/comments.js').start(message, config);
+                    }
+                    if (message.content.startsWith(config.prefix + "level")) {
+                        require('./commands/seelevel.js').start(message, config);
+                    }
                 }
-                break;
-            case "license":
-                license.start(message, config);
-                break;
-            case "kill":
-                kill.start(message, process, config);
-                break;
-            case "reboot":
-                reboot.start(message, client, config);
-                break;
-        }
-        if (message.content.startsWith(config.prefix + "leaderboard")) {
-            leaderboard.start(message, config);
-        }
-        if (message.content.startsWith(config.prefix + "help")) {
-            help.start(message, config);
-        }
-        if (message.content.startsWith(config.prefix + "user")) {
-            userfetch.start(message, config);
-        }
-        if (message.content.startsWith(config.prefix + "comments")) {
-            comments.start(message, config);
-        }
-        if (message.content.startsWith(config.prefix + "level")) {
-            level.start(message, config);
-        }
+            }
+        });
     }
-};
+}
